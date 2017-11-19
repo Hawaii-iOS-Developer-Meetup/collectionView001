c8a3284b76f597bad2197c0e754c8a8cc9e4b761
davidkneely <davidics613@gmail.com>
2017-11-19 07:59:02 -1000

    first commit

---
+++ collectionView001/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  collectionView001
+//
+//  Created by David on 11/18/17.
+//  Copyright © 2017 Vision Runner. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+
+}
